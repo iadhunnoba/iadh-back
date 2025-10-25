f506d9a64678447fb47be6a804e86365ab07902e
FlorenciaMulet <muletmflorencia@gmail.com>
2025-10-25 14:19:37 -0300

    Updated data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "1234",
+    password: "33florencia",
     database: "login_node",
     synchronize: true,
     logging: false,
